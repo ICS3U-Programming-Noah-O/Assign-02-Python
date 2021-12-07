903361f1acf554ecd92fe482380d2282f7522126
Noah-234 <noah.ouellette@stu.ocsb.ca>
2021-12-07 02:19:55 +0000

    Started and completed a python program to calculate the volume and surface area of an icosahedron

--- area_per_icosahedron.py
+++ area_per_icosahedron.py
@@ -1,64 +1,82 @@
 #!/usr/bin/env python3
 # Created By: Noah Ouellette
-# Date: DEC. 2, 2021
+# Date: Dec. 2, 2021
 # This program calculates the volume and surface area of an icosahedron
 
 import math
 import time
+import colorama
+from colorama import Fore, Back, Style
 
 
 def main():
-    # This function asks for the radius and then calculates the area and perimeter.
-  print(" ")
-  print("This program calculates the area and perimeter of an icosahedron!")
+    # This function asks for units and side length and then calculates the
+    # volume and surface area
+    print(Fore.WHITE + "This program calculates the volume "
+          + "and surface area of an icosahedron!")
+    print(" ")
+    time.sleep(2)
+
     # Input
-  units = str(input("Enter the units that you're using: "))
-  edge_length = int(input("Enter the edge length: "))
-  volume_icosahedron = ((5 * (3 + math.sqrt(5))) / 12) * (edge_length ** 3)
-  surface_area_icosahedron = (5 * math.sqrt(3)) * (edge_length ** 2)
-  
-  def surface():
-    print(" ")
-    print("The surface area of your icosahedron is: {}".format (surface_area_icosahedron) + " {}".format(units))
-    print(" ")
-    
-  def volume():
-    print(" ")
-    print("The volume of your icosahedron is: {}".format (volume_icosahedron) + " {}^2".format(units))
-    print(" ")
-    
-   
-    
-  def choice():
-      
-      if(user_choice == "volume"):
-    
-        volume()
-      elif(user_choice == "surface area"):
-      
-          surface()
 
+    units = str(input(Fore.CYAN + "Enter the units that you're using: "))
+    edge_length = int(input("Enter the edge length: "))
 
-  print("Would you like to try to calculate volume, surface area, of total edge length?")
-  user_choice = str(input(" "))
-  choice()
+    # process
 
-  
-  
-  
-    
+    volume_icosahedron = ((5 * (3 + math.sqrt(5))) / 12) * (edge_length ** 3)
+    surface_area_icosahedron = (5 * math.sqrt(3)) * (edge_length ** 2)
+    time.sleep(2)
 
-  
+    # This function displays the surface area
+    def surface():
+        print(" ")
+        print(Fore.WHITE + "The surface area of your "
+              + "icosahedron is: {:,.2f}".format(surface_area_icosahedron)
+              + " {}^2".format(units))
+        print(" ")
 
-  # Ask user if they want to calculate again
-  print("Would you like to try with a different edge length?")
-  user_answer = str(input("Y or N:"))
+    # This function displays the volume
+    def volume():
+        print(" ")
+        print(Fore.WHITE + "The volume of your "
+              "icosahedron is: {:,.2f}".format(volume_icosahedron)
+              + " {}^3".format(units))
+        print(" ")
 
-  if(user_answer == "Y" or "y"):
-    
-    main()
+    # This function allows the user to choose either volume or surface area
+    def choice():
 
-    
+        if(user_choice == "volume"):
+
+            volume()
+        elif(user_choice == "surface area"):
+
+            surface()
+
+        else:
+            print("You must enter either volume or surface area")
+
+    # Calls choice function
+    print(Fore.MAGENTA + "Would you like to try to calculate "
+          + "volume or surface area?")
+    user_choice = str(input(" "))
+    choice()
+
+    # Ask user if they want to calculate again
+    time.sleep(2)
+    print(Fore.BLUE + "Would you like to try with a different edge length?")
+    user_answer = str(input("Y or N:"))
+
+    if(user_answer == "Y"):
+
+        main()
+    if(user_answer == "y"):
+
+        main()
+    else:
+        print(Fore.YELLOW + "Thank you for using this program")
+
 
 if __name__ == "__main__":
     main()
